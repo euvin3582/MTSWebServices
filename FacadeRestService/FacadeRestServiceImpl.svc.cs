ad6387a3bd874fc619cab76088e4b64d8a603dcd
jeuvin <jeuvin@431b5f3f-a2de-ba46-8aff-77102d6902b3>
2014-03-18 20:03:00 +0000

    Adding new middle tear project for mobile -JEuvin
    git-svn-id: https://216.26.176.134:12324/svn/repository/iTraycer/2013PhoneApp/MTSWebServices@3902 431b5f3f-a2de-ba46-8aff-77102d6902b3

---
+++ FacadeRestService/FacadeRestServiceImpl.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceModel;
+using System.Xml;
+
+namespace FacadeRestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FacadeRestServiceImpl" in code, svc and config file together.
+    public class FacadeRestServiceImpl : IFacadeRestServiceImpl
+    {
+        #region IRestService Members
+
+        public string XmlData(string id)
+        {
+            return "Your requested product " + id;
+        }
+
+        public string JsonData(string id)
+        {
+            return "Your requested product " + id;
+        }
+
+        public string JsonDataPost()
+        {
+            string x = OperationContext.Current.RequestContext.RequestMessage.ToString();
+            String CoID = "";
+            String RepID = "";
+
+            XmlDocument payload = new XmlDocument();
+            payload.LoadXml(x);
+            XmlNode nCoID = payload.SelectSingleNode("//CoID");
+            XmlNode nRepID = payload.SelectSingleNode("//RepID");
+
+            if(nCoID!=null)
+                CoID = nCoID.InnerText;
+            if(nRepID!=null)
+                RepID = nRepID.InnerText;
+
+            return "Your requested CoID is: " + CoID + ". Your requested RepID is: " + RepID;
+        }
+
+        #endregion
+    }
+}
---
+++ FacadeRestService/IFacadeRestServiceImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace FacadeRestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFacadeRestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IFacadeRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XmlData(string id);
+
+/*         [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string JsonData(string id);
+*/
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "json/")]
+        string JsonDataPost();
+    }
+}
