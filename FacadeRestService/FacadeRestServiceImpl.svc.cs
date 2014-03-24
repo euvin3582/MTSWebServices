b04bfa235a40ec188fdcf54d0042b06eba6c3a4a
jeuvin <jeuvin@431b5f3f-a2de-ba46-8aff-77102d6902b3>
2014-03-24 20:16:37 +0000

    Fixed web config to work for post and gets -JEuvin
    git-svn-id: https://216.26.176.134:12324/svn/repository/iTraycer/2013PhoneApp/MTSWebServices@3947 431b5f3f-a2de-ba46-8aff-77102d6902b3

--- FacadeRestService/FacadeRestServiceImpl.svc.cs
+++ FacadeRestService/FacadeRestServiceImpl.svc.cs
@@ -17,72 +17,73 @@ namespace FacadeRestService
     {
         public string CreateSession()
         {
-            string bodyRequest = OperationContext.Current.RequestContext.RequestMessage.ToString();
+            return "Start";
+            //string bodyRequest = OperationContext.Current.RequestContext.RequestMessage.ToString();
            
-            XmlDocument payload = new XmlDocument();
-            payload.LoadXml(bodyRequest);
+            //XmlDocument payload = new XmlDocument();
+            //payload.LoadXml(bodyRequest);
 
-            // deserialize payload
-            JsonEnvelope requestEnvelope = new JsonEnvelope();
-            XmlSerializer xmlSer = new XmlSerializer(typeof(JsonEnvelope));
-            requestEnvelope = (JsonEnvelope)xmlSer.Deserialize(new StringReader(payload.OuterXml));
+            //// deserialize payload
+            //JsonEnvelope requestEnvelope = new JsonEnvelope();
+            //XmlSerializer xmlSer = new XmlSerializer(typeof(JsonEnvelope));
+            //requestEnvelope = (JsonEnvelope)xmlSer.Deserialize(new StringReader(payload.OuterXml));
 
-            // build response envelope
-            JsonEnvelope responseEnvelope = new JsonEnvelope();
-            ServiceQueue servieQueue = new ServiceQueue();
-            Parameter parameters = new Parameter();
-            responseEnvelope.ServiceQueues = servieQueue;
-            responseEnvelope.ServiceQueues.Parameters = parameters;
+            //// build response envelope
+            //JsonEnvelope responseEnvelope = new JsonEnvelope();
+            //ServiceQueue servieQueue = new ServiceQueue();
+            //Parameter parameters = new Parameter();
+            //responseEnvelope.ServiceQueues = servieQueue;
+            //responseEnvelope.ServiceQueues.Parameters = parameters;
 
-            // get service name to access
-            string serviceName = requestEnvelope.ServiceQueues.ServiceName;
+            //// get service name to access
+            //string serviceName = requestEnvelope.ServiceQueues.ServiceName;
 
-            if (String.IsNullOrEmpty(serviceName))
-                responseEnvelope.ServiceQueues.ResponseMessage = "No service name was specified";
-            else
-                responseEnvelope.ServiceQueues.ServiceName = serviceName;
+            //if (String.IsNullOrEmpty(serviceName))
+            //    responseEnvelope.ServiceQueues.ResponseMessage = "No service name was specified";
+            //else
+            //    responseEnvelope.ServiceQueues.ServiceName = serviceName;
 
-            switch (serviceName)
-            {
-                case "MTSMobileAuth":
-                    iTraycerDeviceInfo device = new iTraycerDeviceInfo();
-                    String email = "";
-                    String pass = "";
+            //switch (serviceName)
+            //{
+            //    case "MTSMobileAuth":
+            //        iTraycerDeviceInfo device = new iTraycerDeviceInfo();
+            //        String email = "";
+            //        String pass = "";
 
-                    if (!String.IsNullOrEmpty(requestEnvelope.ServiceQueues.ServiceName))
-                    {
-                        device.DeviceId = (requestEnvelope.ServiceQueues).Parameters.DeviceID;
-                        device.DeviceOsVersion = (requestEnvelope.ServiceQueues).Parameters.DeviceOSVersion;
-                        device.Platform = (requestEnvelope.ServiceQueues).Parameters.DevicePlatform;
-                        email = (requestEnvelope.ServiceQueues).Parameters.Email;
-                        pass = (requestEnvelope.ServiceQueues).Parameters.Password;
-                    }
-                    else return "No divice info found";
+            //        if (!String.IsNullOrEmpty(requestEnvelope.ServiceQueues.ServiceName))
+            //        {
+            //            device.DeviceId = (requestEnvelope.ServiceQueues).Parameters.DeviceID;
+            //            device.DeviceOsVersion = (requestEnvelope.ServiceQueues).Parameters.DeviceOSVersion;
+            //            device.Platform = (requestEnvelope.ServiceQueues).Parameters.DevicePlatform;
+            //            email = (requestEnvelope.ServiceQueues).Parameters.Email;
+            //            pass = (requestEnvelope.ServiceQueues).Parameters.Password;
+            //        }
+            //        else return "No divice info found";
             
-                    // before you run this we need to pass a device to the json envelope to fill in the device info 
-                    string[] resposne = iTarycerSection.Session.Session.CreateUserSession(email, pass, device);
+            //        // before you run this we need to pass a device to the json envelope to fill in the device info 
+            //        string[] resposne = iTarycerSection.Session.Session.CreateUserSession(email, pass, device);
 
-                    if (resposne.Length > 0)
-                    {
-                        responseEnvelope.CoID = resposne[0];
-                        responseEnvelope.RepID = resposne[1];
-                        responseEnvelope.MtsToken = resposne[2];
-                        responseEnvelope.ServiceQueues.ResponseMessage = "Succesfully authenticated user";
-                        responseEnvelope.Commit = "true";
-                    }
-                    else
-                    {
-                        responseEnvelope.ServiceQueues.ResponseMessage = "Failed to authenticate user";
-                        responseEnvelope.Commit = "false";
-                    }
-                    break;
+            //        if (resposne.Length > 0)
+            //        {
+            //            responseEnvelope.CoID = resposne[0];
+            //            responseEnvelope.RepID = resposne[1];
+            //            responseEnvelope.MtsToken = resposne[2];
+            //            responseEnvelope.ServiceQueues.ResponseMessage = "Succesfully authenticated user";
+            //            responseEnvelope.Commit = "true";
+            //        }
+            //        else
+            //        {
+            //            responseEnvelope.ServiceQueues.ResponseMessage = "Failed to authenticate user";
+            //            responseEnvelope.Commit = "false";
+            //        }
+            //        break;
 
-                case "MTSOther":
-                    break;
-            }
+            //    case "MTSOther":
+            //        break;
+            //}
 
-            responseEnvelope.SyncResponseTime = DateTime.UtcNow.ToString();
-            return JsonConvert.SerializeObject(responseEnvelope);
+            //responseEnvelope.SyncResponseTime = DateTime.UtcNow.ToString();
+            //return JsonConvert.SerializeObject(responseEnvelope);
            
         }
     }
