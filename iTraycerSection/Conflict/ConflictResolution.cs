e29f7e6ca93247f4328a6d9d3f2b79b3c7d4b149
jeuvin <jeuvin@431b5f3f-a2de-ba46-8aff-77102d6902b3>
2014-05-31 21:04:28 +0000

    Fixed conflict resolution duplicate case bug -JEuvin
    git-svn-id: https://216.26.176.134:12324/svn/repository/iTraycer/2013PhoneApp/MTSWebServices@4706 431b5f3f-a2de-ba46-8aff-77102d6902b3

--- iTraycerSection/Conflict/ConflictResolution.cs
+++ iTraycerSection/Conflict/ConflictResolution.cs
@@ -10,10 +10,6 @@ namespace iTraycerSection.Conflict
 {
     public class ConflictResolution
     {
-        
-
-        
-
         public static bool CheckForConflict(String conflictType, Object obj)
         {
             bool isConflict = false;
@@ -21,7 +17,7 @@ namespace iTraycerSection.Conflict
             switch(conflictType){
                 // Same doctor, same location, same time - Conflict gets resolved by creation date
                 case "SCheduledConflict":
-                    DataTable scheduleConflict = DataLayer.Controller.GetAllKitTrayUsageDatesByCompanyId(((ScheduleInfo)obj).CompanyId); 
+                    DataTable scheduleConflict = DataLayer.Controller.GetAllKitTrayUsageDatesByCompanyId(((ScheduleInfo)obj).CompanyId);
                     isConflict =  SCheduledConflict(scheduleConflict, (ScheduleInfo)obj, "MTSSurgeryDetails", "RepId");
                     break;
 
@@ -34,6 +30,11 @@ namespace iTraycerSection.Conflict
         // Same doctor, same location, same time
         private static bool SCheduledConflict(DataTable schedules, ScheduleInfo obj, String tableName, String conflictColumn)
         {
+            // remove duplicates
+            DataView view = new DataView(schedules);
+            schedules = view.ToTable(true, "CaseId", "RepID", "DoctorId", "LocationID",	"SurgeryDate", "ModifiedDate");
+
+            // find conflicting rows
             DataRow[] scheduleRows = schedules.Select(String.Format("DoctorId = '{0}' AND LocationID = '{1}' AND SurgeryDate = '{2}'", obj.SurgeonId, obj.HospitalInfo.Id, obj.SurgeryDate));
 
             if (scheduleRows.Length == 0)
